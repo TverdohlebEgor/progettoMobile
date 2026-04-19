5be8564832496172ae9d48e26209fb74426424b4
Tverdohleb Egor <etverdohleb@imolainformatica.it>
2026-04-19 09:25:31 +0200

    Now you can start the backend from docker

--- frontend/app/src/main/java/cohappy/frontend/model/dto/request/RegisterDTO.kt
+++ frontend/app/src/main/java/cohappy/frontend/model/dto/request/RegisterDTO.kt
@@ -1,8 +1,13 @@
 package cohappy.frontend.model.dto.request
+
 import com.squareup.moshi.JsonClass
 
 @JsonClass(generateAdapter = true)
 data class RegisterDTO(
+    val name: String,
+    val surname: String,
+    val birthDate: String,
+    val cf: String,
     var images: List<ByteArray>? = null,
     val email: String,
     val phoneNumber: String,
