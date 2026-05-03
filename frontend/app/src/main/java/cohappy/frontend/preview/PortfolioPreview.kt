45d136a80bb959d238a3e0314cdcf2adb136856c
Tverdohleb Egor <etverdohleb@imolainformatica.it>
2026-05-03 12:41:26 +0200

    Release new jar

--- frontend/app/src/main/java/cohappy/frontend/preview/PortfolioPreview.kt
+++ frontend/app/src/main/java/cohappy/frontend/preview/PortfolioPreview.kt
@@ -9,11 +9,33 @@ import cohappy.frontend.view.house.PortfolioView
 
 @Preview(showBackground = true)
 @Composable
-fun PreviewPortfolio() {
+fun PreviewPortfolioInDebt() {
     MaterialTheme {
         PortfolioView(
             isLoading = false,
             totalDebts = -15.50,
+            totalCredits = 24.50,
+            activeFilter = "ALL",
+            transactions = listOf(
+                PortfolioTransaction("1", false, "Spesa Esselunga", "Oggi • Hai pagato tu", 24.50),
+                PortfolioTransaction("2", true, "Bolletta Luce", "Ieri • Ha pagato Marco", 15.00),
+                PortfolioTransaction("3", true, "Sushi Delivery", "Ven 12 • Ha pagato Sofia", 22.00),
+                PortfolioTransaction("4", false, "Detersivi e Saponi", "Mer 10 • Hai pagato tu", 8.00)
+            ),
+            onFilterChange = {},
+            userToken = ""
+        )
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewPortfolioInCredit() {
+    MaterialTheme {
+        PortfolioView(
+            isLoading = false,
+            totalDebts = 0.0,
             totalCredits = 24.50,
             activeFilter = "ALL",
             transactions = listOf(
