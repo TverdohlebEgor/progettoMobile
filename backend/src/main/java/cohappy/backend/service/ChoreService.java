645247684c9725d63055204c1a950164a9b1cba2
Tverdohleb Egor <etverdohleb@imolainformatica.it>
2026-05-20 20:25:55 +0200

    Improved errore messaeg

--- backend/src/main/java/cohappy/backend/service/ChoreService.java
+++ backend/src/main/java/cohappy/backend/service/ChoreService.java
@@ -151,7 +151,7 @@ public class ChoreService {
 
         for (LocalDate day : createChoreDTO.getDays()) {
             if (!createChoreDTO.getAssignedTo().containsKey(day)) {
-                throw new IllegalInputException("A day of the chore has not assigned user");
+                throw new IllegalInputException("A day of the chore has not assigned user, day:'%s'".formatted(day));
             }
 
             String userCode = createChoreDTO.getAssignedTo().get(day);
